5d59c48db82a35547560867f4cb3fa4d62e2a912
pkoenig077 <pkoenig077@gmail.com>
2016-02-08 09:52:07 -0500

    completed movie db

--- MovieList/src/MainMovieList.java
+++ MovieList/src/MainMovieList.java
@@ -6,34 +6,37 @@ import java.util.Collections;
  * @author Phil
  *
  *
- *	Stores a list of movies and displays them by category
- *	users can enter a category eg (Animated, Drama, Horror, Sci-fi)
- *prompt for another search
- *@SPECS
- *each movie should be represented by an object of category movie
- *movie class must provide 2 private fields Title and Category
- *movie class should provide a constructor that accepts Title and Category
- *when a user enters a category program should read through all of the movies in the Map/list and display a line for all movies that match the category entered
- *Validate user inputs
- *Standardize category codes by displaying a menu of categories and having the user enter category by number instead of name
- *Display movies sorted alphabetically
+ *         Stores a list of movies and displays them by category users can enter
+ *         a category eg (Animated, Drama, Horror, Sci-fi) prompt for another
+ *         search
+ * @SPECS each movie should be represented by an object of category movie movie
+ *        class must provide 2 private fields Title and Category movie class
+ *        should provide a constructor that accepts Title and Category when a
+ *        user enters a category program should read through all of the movies
+ *        in the Map/list and display a line for all movies that match the
+ *        category entered Validate user inputs Standardize category codes by
+ *        displaying a menu of categories and having the user enter category by
+ *        number instead of name Display movies sorted alphabetically
  *
  */
 public class MainMovieList {
 	public static void main(String[] args) {
 		ArrayList<Movie> movieList = new ArrayList<>();
-		for (int i=0;i<100;i++){
-		movieList.add(MovieIO.getMovie(i+1));
-		Collections.sort(movieList);
+		for (int i = 0; i < 100; i++) {
+			movieList.add(MovieIO.getMovie(i + 1));
+			Collections.sort(movieList);
 		}
-		
-	
-		for(int i=0;i<movieList.size();i++){
-		Collections.sort(movieList);
-		System.out.println(movieList.get(i).getTitle());
+		boolean restart = true;
+		while (restart == true) {
+			String genre = Validate.getCatInput("Enter a genre: ");
+			
+			for (int i = 0; i < movieList.size(); i++) {
+				if(genre.equalsIgnoreCase(movieList.get(i).getCategory())){
+				System.out.println(movieList.get(i).getTitle());
+				}
+			}
+			restart = Validate.getRestart();
 		}
-		
-	
+		System.out.println("Good Bye");
 	}
-	
 }
--- MovieList/src/Validate.java
+++ MovieList/src/Validate.java
@@ -1,4 +1,54 @@
+import java.util.Scanner;
 
 public class Validate {
+	static Scanner sc = new Scanner(System.in);
+
+
+	/**
+	 * 
+	 * @return a boolean restart checks if inputted string for a yes/no prompt
+	 */
+	public static boolean getRestart() {
+		String prompt = getRestartInput("\nWould you like to restart?\n(Y)es/(N)o");
+		boolean restart = true;
+		while (!(prompt.matches("^[YyNn]$") || prompt.equalsIgnoreCase("yes") || prompt.equalsIgnoreCase("no"))) {
+			System.out.println("Error: Invalid Input");
+			prompt = getRestartInput("\nWould you like to restart?\n(Y)es/(N)o");
+		}
+		if (prompt.equalsIgnoreCase("y") || prompt.equalsIgnoreCase("yes")) {
+			restart = true;
+		} else if (prompt.equalsIgnoreCase("n") || prompt.equalsIgnoreCase("no")) {
+			restart = false;
+		}
+		return restart;
+
+	}
+
+	/**
+	 * 
+	 * @return string restart gets a string input for the getRestart method
+	 */
+	private static String getRestartInput(String prompt) {
+
+		System.out.println(prompt);
+		String restart = sc.nextLine();
+		return restart;
+	}
+
+	public static String getInput(String prompt) {
+		System.out.println(prompt);
+		return sc.nextLine();
+	}
+
+	public static String getCatInput(String prompt) {
+		String catInput;
+		catInput = getInput(prompt);
+		while(!(catInput.equalsIgnoreCase("Animated")||catInput.equalsIgnoreCase("Comedy")||catInput.equalsIgnoreCase("Drama")||catInput.equalsIgnoreCase("Horror")||catInput.equalsIgnoreCase("Scifi")||catInput.equalsIgnoreCase("musical"))){
+			System.out.println("No movies of that genre: ");
+			catInput = getCatInput(prompt);
+		}
+		
+		return catInput;
+	}
 
 }
