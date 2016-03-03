5c71469a789ef0242d8984b94e64e564ce6d6406
pkoenig077 <pkoenig077@gmail.com>
2016-03-03 11:06:24 -0500

    working restful service example

--- InClassPractice/3216/src/com/customerService/list/cService.java
+++ InClassPractice/3216/src/com/customerService/list/cService.java
@@ -1,13 +1,29 @@
 package com.customerService.list;
 
-import javax.ws.rs.*;
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/list")
+@Path("/list") // service call mapping 
 public class cService {
-@GET
-@Produces(MediaType.TEXT_PLAIN)
-public String listService(){
-	return "Hello World";
-}
+    @GET // Http verb 
+    @Produces(MediaType.APPLICATION_JSON)// media type
+    public ArrayList<customer> returnTitle()
+    {
+        customer s= new customer();
+        s.setName("John");
+        s.setID("1234");
+        
+        customer d = new customer();
+        d.setID("4321");
+        d.setName("Phil");
+        
+        ArrayList<customer> ar = new ArrayList<>();
+        ar.add(d);
+        ar.add(s);
+        return ar;
+    }
 }
--- InClassPractice/3216/src/com/customerService/list/customer.java
+++ InClassPractice/3216/src/com/customerService/list/customer.java
@@ -2,6 +2,11 @@ package com.customerService.list;
 
 import java.io.Serializable;
 
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.sun.xml.internal.txw2.annotation.XmlElement;
+@XmlRootElement(name = "customer")
 public class customer implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -11,7 +16,7 @@ public class customer implements Serializable{
 	public String getName() {
 		return Name;
 	}
-
+	 @XmlElement
 	public void setName(String name) {
 		Name = name;
 	}
@@ -19,7 +24,7 @@ public class customer implements Serializable{
 	public String getID() {
 		return ID;
 	}
-
+	 @XmlElement
 	public void setID(String iD) {
 		ID = iD;
 	}
